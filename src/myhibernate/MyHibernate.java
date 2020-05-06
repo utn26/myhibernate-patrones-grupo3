b43646d5d06254b4b63029a826a8d77ac30300d3
utn26 <32240126+utn26@users.noreply.github.com>
2020-05-06 17:35:56 -0300

    agregando estructura de funcion find

--- src/myhibernate/MyHibernate.java
+++ src/myhibernate/MyHibernate.java
@@ -1,28 +1,49 @@
 package myhibernate;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
+import myhibernate.connect.ConnectorSQL;
 import myhibernate.demo.Producto;
 
 public class MyHibernate
 {
-   public static <T> T find(Class<T> clazz, int id)
-   {
-      // PROGRAMAR AQUI 
-	   
-      return null;
-   }
+	public static <T> T find(Class<T> clazz, int id)
+	{
+		// PROGRAMAR AQUI
+		// 1. generar la query string
+		// generateQuery(Class<T> clazz, int id);
+		Connection connection=ConnectorSQL.conectar();
+		Class<T> ObjectResult = null;
+		try
+		{
+			Statement statement=(Statement)connection.createStatement();
+//			ResultSet resultset = statement.executeQuery(generateQuery(Class<T> clazz, int id));
+//			//2.generamos el objeto con el resultado de la query
+//			ObjectResult = loadObject(resultset);
+//			
+		}
+		catch(SQLException e)
+		{
+			throw new RuntimeException();
+		}
+		
+		return (T)ObjectResult;
+	}
 
-   public static <T> List<T> findAll(Class<T> clazz)
-   {
-      // PROGRAMAR AQUI
-      return null;
-   }
+	public static <T> List<T> findAll(Class<T> clazz)
+	{
+		// PROGRAMAR AQUI
+		return null;
+	}
 
-   public static Query createQuery(String hql)
-   {
-      // PROGRAMAR AQUI
-      return null;
-   }
+	public static Query createQuery(String hql)
+	{
+		// PROGRAMAR AQUI
+		return null;
+	}
 
 }
